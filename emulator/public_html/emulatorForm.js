28f033946d598d0cf623eb356b3255aac3c931af
Chontira <chontiran@gmail.com>
2015-04-30 13:25:36 +1200

    Added textarea for loading messages and link emulatorForm and index html together

--- emulator/public_html/emulatorForm.js
+++ emulator/public_html/emulatorForm.js
@@ -1,7 +1,15 @@
 /* 
-This page is use to load process information from index.html
+This page is use to load processed information from index.html
 It select the right file and load that file on to the canvas in html
  */
 
+$('#load').on('click', function(){
+    
+        var appSelected = $('#AppSelection :selected');
+	var appText = appSelected.text();
+	$('#text').val("Loading " + appText);
+	
+        
+ });
 
 
