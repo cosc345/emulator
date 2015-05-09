2163bf155c7b1a83ea9149937a28a9257ae86cfd
Chontira <chontiran@gmail.com>
2015-05-09 20:22:33 +1200

    Working on the collection for medicine

---
+++ emulator/public_html/HealthApp/Medicine/MedicineOb.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function MedicineOb(name, quantity, person){
+    this.medName = name;
+    this.medquantity =  quantity;
+    this.client = person;
+    var medicines = new Array();
+    
+    Me
+}
