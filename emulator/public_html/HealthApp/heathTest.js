fbca5c2d5b0278a03a5ae5206b72c6bcefa7c1d0
nimch681 <nimch681@chontira.telecom>
2015-05-17 01:29:02 +1200

    Got the interface of the heart simulator right

--- emulator/public_html/HealthApp/heathTest.js
+++ emulator/public_html/HealthApp/heathTest.js
@@ -11,25 +11,43 @@ var x;
 $(document).ready(function () {
     
     paper.setup(canvas);
-    x = getRandomInt(50, 120);
+    x = heartRateGen();
+    $(textarea).val(x);
+    console.log(x);
     ctx.font = "20px Georgia";
     ctx.fillText("Heart Rate", 57, 50);
     ctx.font = "50x Georgia";
     ctx.fillText(x.toString(), 85, 100);
-    heartRateGen();
-    setTimeout(function(){  RunningTheApp("HealthApp/heathTest.js") }, 1500);
+    if (x < 50) {
+        
+            ctx.font = "50x Georgia";
+            ctx.fillText("Emergency", 60, 125);
+            ctx.fillText("Call 111 immediately", 17, 150);
+       
+
+
+    }
+    if (x > 120) {
+             ctx.font = "50x Georgia";
+            ctx.fillText("Emergency", 60, 125);
+             ctx.fillText("Call 111 immediately", 17, 150);
+       
+
+
+    }
+
+    setTimeout(function () {
+        RunningTheApp("HealthApp/heathTest.js")
+    }, 1500);
 
 
 });
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function onResize(event) {
-    // Whenever the window is resized, recenter the path:
-    path.position = view.center;
-};
+
 function chance() {
-    xchance = (Math.random() < 0.99 ? 0 : 1);
+    xchance = (Math.random() < 0.90 ? 0 : 1);
     console.log("chance is " + xchance);
     return xchance;
 }
@@ -50,36 +68,30 @@ function drawHeartRate(){
 //Generate heart rate at random using the chance() 
 //if the generator show somethind greater than 120 or less than 50. It will warned you to call 111
   function heartRateGen() {
-        $(textarea).val(x);
-        console.log(x);
-        if (chance() === 0) {
-            x = getRandomInt(50, 120);
+        
+        if (chance() === 0) { 
             console.log("Heartrate is between 50 to 100");
+            return x = getRandomInt(50, 120);
+           
             
-           // setTimeout(incrementCounter, 1500);
+           
         } else {
             console.log("Heartrate is beyone 50 or 100");
             x = getRandomInt(1, 200);
-            //ctx.fillText(x+"", 10, 50);
 
-            if (x < 50) {
-                x = getRandomInt(1, 50);
-                //ctx.fillText(x+"", 10, 50);
-                //setInterval(function () {
-                  $(textarea).val(x + " is a dangerous heartbeat, call 111 immediately");
-               // }, 1500);
+            if (x < 85) {
+               return x = getRandomInt(1, 50);
+                
 
             } else {
-                if (x > 100) {
-                    x = getRandomInt(120, 200);
-                    //ctx.fillText(x+"", 10, 50);
-                    //setInterval(function () {
-                      $(textarea).val(x + " is a dangerous heartbeat, call 111 immediately");
-                   // 1500);
+                if (x > 85) {
+                    return x = getRandomInt(120, 200);
+                    
 
                 }
             }
         }
+        return  85;
         
     }
  
