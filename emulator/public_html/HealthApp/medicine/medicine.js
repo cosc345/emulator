8e34a6ddf4e6020e3abfa4705f439af9988e3593
LinlinWang <LinlinWang@10.1.1.2>
2015-08-09 21:58:34 +1200

    Reading JSON from external file

--- emulator/public_html/HealthApp/medicine/medicine.js
+++ emulator/public_html/HealthApp/medicine/medicine.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,13 +8,13 @@ $(document).ready(function () {
     var message = "Loading the medicine notification";
     $('#text').val(message);
     console.log(message);
+
     //digital clock from http://www.anthoniraj.com/digital-clock-using-html5-canvas-and-javascript-js/
     var context;
     var d;
     var str;
     function getClock()
     {
-
         //Get Current Time
         d = new Date();
         str = prefixZero(d.getHours(), d.getMinutes(), d.getSeconds());
@@ -30,6 +30,7 @@ $(document).ready(function () {
 
     }
 
+    //sets the digital format of the clock
     function prefixZero(hour, min, sec)
     {
         var curTime;
@@ -49,10 +50,17 @@ $(document).ready(function () {
             curTime += ":" + sec.toString();
         return curTime;
     }
-
+    
     setInterval(getClock, 1000);
-    setInterval(function(){alert("Take Medicine")},5000);
 
 });
 
+$.getJSON('data.json', function(data){
+    var output="<ul>";
+    for (var i in data.users){
+        output+="<li>" + data.users[i].name + "</br>" + data.users[i].dosage + "</br>" + data.users[i].practice+"</li>";
+    }
+    output+="</ul>";
     
+    setInterval(function(){alert(data)},5000);
+});
